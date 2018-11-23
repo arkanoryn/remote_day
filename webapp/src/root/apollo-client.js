b12c0cb56f53a180ccfd52e50cb054ab83904fa4
Pierre-Nicolas Sormani <arkanoryn@gmail.com>
2018-11-23 16:58:38 +0100

    Add Gravatar component

--- webapp/src/root/apollo-client.js
+++ webapp/src/root/apollo-client.js
@@ -1,5 +1,11 @@
 import ApolloClient from 'apollo-boost';
 
-const client = new ApolloClient({ uri: 'http://localhost:4000/v1' });
+const token = localStorage.getItem('token');
+console.log('....................... apollo-client#token:', token);
+
+const client = new ApolloClient({
+  uri:     'http://localhost:4000/v1',
+  headers: { authorization: token ? `Bearer ${token}` : '' },
+});
 
 export default client;
